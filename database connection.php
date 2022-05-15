4ed89a48e89fb94f8c42573f73107f04d5166659
netra2060002 <np03cs4s210099@heraldcollege.edu.np>
2022-05-15 19:05:00 +0545

    add new file database connection.php etc

--- database connection.php
+++ database connection.php
@@ -1,9 +1,12 @@
 <?php
 
+//con is variable for database connection
 
 $con = mysqli_connect('localhost','root');
 
 mysqli_select_db($con,'water');
+
+/*  when database is unable to connect, it show message that connection can't be estblished */
 
 if($con->connect_error){
 	die("Connection can't be established: ".$con->connect_error);
